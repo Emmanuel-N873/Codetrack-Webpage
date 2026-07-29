f63bb51831a5ab1c0693e85b6d30091925b5b69e
Emmanuel Nwauzobilom <enwauzobilom@gmail.com>
2026-07-29 17:19:36 +0100

    feat(JS_Assignment 1)Update on my js assignment

---
+++ js_assignment.js
@@ -0,0 +1,106 @@
+//Question 1//
+const FullName = "Emmanuel Nwauzobilom";
+let age = 31;
+const isEnrolled = true;
+console.log(FullName);
+console.log(age);
+console.log(isEnrolled);
+
+// This gives the data and status of the user.
+
+//Question 2//
+let walletBalance = 5000;
+console.log("Initial Balance:", walletBalance);
+walletBalance = 7500;
+console.log("Updated Balance:", walletBalance);
+// This shows the initial and updated balance of the wallet.
+
+//Question 3//
+const ItemInCart = 5;
+const ItemPrice = 2.50;
+console.log(ItemInCart);
+console.log(ItemPrice);
+// This shows the total number of items in the cart as 5 and the price per item as 2.50.
+
+//Question 4//
+const price = 2500;
+const quantity = 4;
+const totalCost = price * quantity;
+console.log(totalCost);
+// This shows the total cost of the items.
+
+//Question 5//
+const a = 17;
+const b = 5;
+
+const sum = a + b;
+console.log(sum);
+// This shows the sum of a and b.
+const substract = a - b;
+console.log(substract);
+// This shows the difference between a and b.
+const multiply = a * b;
+console.log(multiply);
+// This shows the product of a and b.
+const divide = a / b;
+console.log(divide);
+// This shows the quotient of a and b.
+const modulus = a % b;
+console.log(modulus);
+// This shows the remainder of a divided by b.
+const exponent = a ** b;
+console.log(exponent);
+// This shows the result of a raised to the power of b.
+
+// This shows the result of various arithmetic operations between a and b.//
+
+//Question 6//
+const SellingPrice = 12000;
+const DeliveryCost = 2000;
+const QuantitySold = 3;
+const Income = (SellingPrice + DeliveryCost) * QuantitySold;
+console.log(Income);
+// This shows the total income from selling 3 items at a selling price of 12000 and a delivery cost of 2000 each.//
+
+//Question 7//
+const StudentScore = 68;
+const PassMark = 50;
+const greater = StudentScore > PassMark;
+console.log(greater);
+console.log(StudentScore >= 70);
+// This shows whether the student score is greater than the pass mark and whether it is greater than or equal to 70.
+
+//Question 8//
+const priceA = 8000;
+const priceB = 12000;
+
+const lessthan = priceA < priceB;
+const greaterthan = priceA > priceB;
+const lessthanorequal = priceA <= priceB;
+const greaterthanorequal = priceA >= priceB;
+
+console.log(lessthan);
+console.log(greaterthan);
+console.log(lessthanorequal);
+console.log(greaterthanorequal);
+//This shows vaious comparisons between piceA and priceB
+
+//Question 9
+const num = 7;
+const str = "7";
+const loose = num == str;
+const strict = num === str;
+console.log(loose)
+console.log(strict)
+// The loose equality shows a comparism between the two values for equality of the numbers, 
+// While strict equality compares both the value and the data type without changing them.
+
+//Question 10
+const e = 7;
+const f = "7";
+const notLoose = e != f;
+const notStrict = e !== f;
+
+console.log(notLoose);
+console.log(notStrict);
+
