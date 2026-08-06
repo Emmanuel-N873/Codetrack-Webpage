81a1e66b7ab662c5d34d9e2b136bcebeaa971db4
Emmanuel Nwauzobilom <enwauzobilom@gmail.com>
2026-08-06 07:54:33 +0100

    feat:Personal practice

--- Practice.js
+++ Practice.js
@@ -1,8 +1,38 @@
-switch (key) {
-    case value:
-        
-        break;
+// let name = "John";
+// console.log(name);
 
-    default:
-        break;
+// let firstName = "Emmanuel";
+// console.log(firstName);
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// //Primitive Types:
+// let person = {
+//     Name: "Emmanuel",
+//     Age: 20
+// };
+// console.log(person);
+
+// //Dot Notation
+// person.Name = 'Emmanuel';
+// console.log(person.name);
+
+// //Bracket Notation
+// person['Name'] = 'Emmanuel';
+// console.log(person);
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'green';
+// selectedColors[3] = 1;
+// console.log(selectedColors.length);
+
+//Performing a task
+function greet(name, lastname) {
+    console.log("Hello, " + name + " " + lastname);
 }
+
+greet("Emmanuel", "Nwauzobilom");
+greet("Peculiar", "Otu");
+
