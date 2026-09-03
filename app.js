30187f4b43cdf3fea2ef55597c32510eef22b433
enwauzobilom@gmail.com <enwauzobilom@gmail.com>
2026-09-03 16:29:24 +0100

    feat: Dom Manipulation Updated

--- app.js
+++ app.js
@@ -10,16 +10,32 @@
 // console.dir(headingOfPage);
 // console.log(headingOfPage.textContent);
 
-const heroPrimaryButton = document.getElementById("hero_pri")
-heroPrimaryButton.textContent = "Ten Ten Naira"
-heroPrimaryButton.style.backgroundColor = "Black"
-console.dir(heroPrimaryButton);
+// const heroPrimaryButton = document.getElementById("hero_pri")
+// heroPrimaryButton.textContent = "Ten Ten Naira"
+// heroPrimaryButton.style.backgroundColor = "Black"
+// console.dir(heroPrimaryButton);
 
-//When you have alot of things you want to change all at once.
-const allPtags = document.querySelectorAll("p");
+// //When you have alot of things you want to change all at once.
+// const allPtags = document.querySelectorAll("p");
 
-allPtags.forEach((ptag) => {
-    ptag.textContent = "Na me dey run am"
-    console.log((ptag.textContent = "Na me dey run am"));
+// allPtags.forEach((ptag) => {
+//     ptag.textContent = "Na me dey run am"
+//     console.log((ptag.textContent = "Na me dey run am"));
+// });
+// console.log(allPtags);
+
+//Event driven manipulation
+const heroPrimaryButton = document.getElementById("hero_pri");
+const mainTitle = document.getElementById("mainTitle");
+
+heroPrimaryButton.addEventListener("click", () => {
+    //Button has been clicked
+    heroPrimaryButton.textContent = "I have been clicked";
+    heroPrimaryButton.style.backgroundColor = "blue";
+    heroPrimaryButton.style.color = "green";
+
+    mainTitle.textContent = "I Love JavaScript";
+
+    //Alert the user that the button has been clicked.
+    alert("Something has changed on the page. Check it out!");
 });
-console.log(allPtags);
