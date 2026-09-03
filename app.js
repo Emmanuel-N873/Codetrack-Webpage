c65b7dd05806adb1d93d9af484e6b960a8bd0512
enwauzobilom@gmail.com <enwauzobilom@gmail.com>
2026-09-03 15:56:32 +0100

    feat: Dom Manipulation

---
+++ app.js
@@ -0,0 +1,25 @@
+// console.log("Browser Console Check")
+
+//Document Object Model (DOM) Manipulation
+//Note: Every element in the HTML document is represented as an object in the DOM. The DOM allows us to access and manipulate these elements using JavaScript.
+//body element are known as "document"
+
+// const headingOfPage = document.querySelector("h1");
+// // headingOfPage.textContent = "I love JavaScript";
+
+// console.dir(headingOfPage);
+// console.log(headingOfPage.textContent);
+
+const heroPrimaryButton = document.getElementById("hero_pri")
+heroPrimaryButton.textContent = "Ten Ten Naira"
+heroPrimaryButton.style.backgroundColor = "Black"
+console.dir(heroPrimaryButton);
+
+//When you have alot of things you want to change all at once.
+const allPtags = document.querySelectorAll("p");
+
+allPtags.forEach((ptag) => {
+    ptag.textContent = "Na me dey run am"
+    console.log((ptag.textContent = "Na me dey run am"));
+});
+console.log(allPtags);
